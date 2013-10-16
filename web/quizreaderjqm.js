45faec847d9107d22159b031628042df635e77b6
jhammen@gmail.com <jhammen@gmail.com>
2013-10-16 15:14:22 -0600

    show open titles + basic library

--- web/quizreaderjqm.js
+++ web/quizreaderjqm.js
@@ -1,7 +1,23 @@
+/*
+ This file is part of QuizReader.
+
+ QuizReader is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ QuizReader is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with QuizReader.  If not, see <http://www.gnu.org/licenses/>.
+ */
 var qr = {
 
 	get : function(url) {
-		return $.get(url, "html").fail(function(jxhr, status, error) {
+		return $.get(url).fail(function(jxhr, status, error) {
 			alert(error);
 			console.log(error);
 		});
@@ -11,9 +27,30 @@ var qr = {
 $(document).delegate("#splash", "pageinit", function() {
 	var source = $("#splash_template").html();
 	var template = Handlebars.compile(source);
-	
+
 	$(document).on('pagebeforeshow', '#splash', function(e, data) {
-		var data = [{language: "German", count: 99 }, {language: "Spanish", count: 101 }];
+		var data = dao.getWordCounts();
 		$("#language_list").html(template(data)).listview("refresh");
+	});
+});
+
+$(document).delegate("#current", "pageinit", function() {
+	var source = $("#current_template").html();
+	var template = Handlebars.compile(source);
+
+	$(document).on('pagebeforeshow', '#current', function(e, data) {
+		var data = dao.getOpenTitles();
+		$("#current_list").html(template(data)).listview("refresh");
+	});
+});
+
+$(document).delegate("#library", "pageinit", function() {
+	var source = $("#library_template").html();
+	var template = Handlebars.compile(source);
+
+	$(document).on('pagebeforeshow', '#library', function(e, data) {
+		$.getJSON("/library/fr/titles.json").done(function(data) {
+			$("#library_list").html(template(data)).listview("refresh");
+		});
 	});
 });
---
+++ web/test/dao.js
@@ -0,0 +1,21 @@
+var dao = {
+	getWordCounts : function() {
+		return [ {
+			language : "German",
+			count : 99
+		}, {
+			language : "Spanish",
+			count : 101
+		} ];
+	}
+
+	,
+	getOpenTitles : function() {
+		return [ {
+			title : "Here is the Title",
+			author: "author",
+			loc: "/library/test"
+		} ];
+	}
+
+};
