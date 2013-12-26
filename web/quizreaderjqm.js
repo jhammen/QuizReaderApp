e1c709745ca56670a5bf12a15c70f124ac801637
jhammen <jhammen@gmail.com>
2013-12-26 15:46:33 -0700

    navigate thru library folders

--- web/quizreaderjqm.js
+++ web/quizreaderjqm.js
@@ -16,21 +16,54 @@
  */
 var qr = {
 
+	language : null,
+
+	library : null,
+
 	get : function(url) {
 		return $.get(url).fail(function(jxhr, status, error) {
 			alert(error);
 			console.log(error);
 		});
-	}
+	},
+
+	showCurrent : function(code) {
+		this.language = code;
+		this.library = "/library/" + code;
+		$.mobile.changePage("#current");
+	},
+
+	showTitle : function(id) {
+		alert("showing title " + id);
+	},
+
 };
 
+// global init for any page
+$(document).delegate("div[data-role='page']", "pageinit", function() {
+	if (!qr.language) {
+		Handlebars.registerPartial("title", $("#title_template").html());
+		$.mobile.changePage("#splash");
+	}
+
+	$(document).on("pagebeforeshow", "div[data-role='page']", function(e, data) {
+		// 
+	});
+});
+
 $(document).delegate("#splash", "pageinit", function() {
+	// init for splash page
 	var source = $("#splash_template").html();
 	var template = Handlebars.compile(source);
 
 	$(document).on('pagebeforeshow', '#splash', function(e, data) {
 		var data = dao.getWordCounts();
-		$("#language_list").html(template(data)).listview("refresh");
+		var list = $("#language_list");
+		list.html(template(data)).listview("refresh");
+		$("a[data-code]", list).on('click', function(e) {
+			e.preventDefault();
+			qr.showCurrent($(this).data("code"));
+		});
 	});
 });
 
@@ -48,9 +81,26 @@ $(document).delegate("#library", "pageinit", function() {
 	var source = $("#library_template").html();
 	var template = Handlebars.compile(source);
 
-	$(document).on('pagebeforeshow', '#library', function(e, data) {
-		$.getJSON("/library/fr/titles.json").done(function(data) {
-			$("#library_list").html(template(data)).listview("refresh");
+	function showEntry() {
+		$.getJSON(qr.library + "/idx.json").done(function(data) {
+			var list = $("#library_list");
+			list.html(template(data)).listview("refresh");
+			$("li[data-title]", list).on('click', function(e) {
+				e.preventDefault();
+				qr.showTitle($(this).data("title"));
+			});
+			$("a[data-path]", list).on('click', function(e) {
+				e.preventDefault();
+				qr.library = qr.library + "/" + $(this).data("path")
+				showEntry();
+			});
+		}).fail(function(foo, mesg) {
+			alert(mesg);
+			console.log(foo);
 		});
+	}
+
+	$(document).on('pagebeforeshow', '#library', function(e, data) {
+		showEntry();
 	});
 });
--- web/test/dao.js
+++ web/test/dao.js
@@ -1,20 +1,22 @@
 var dao = {
+
 	getWordCounts : function() {
 		return [ {
 			language : "German",
+			code : "de",
 			count : 99
 		}, {
 			language : "Spanish",
+			code : "es",
 			count : 101
 		} ];
-	}
+	},
 
-	,
 	getOpenTitles : function() {
 		return [ {
-			title : "Here is the Title",
-			author: "author",
-			loc: "/library/test"
+			title : "Here is an Open Title",
+			author : "the author",
+			loc : "/library/??"
 		} ];
 	}
 
